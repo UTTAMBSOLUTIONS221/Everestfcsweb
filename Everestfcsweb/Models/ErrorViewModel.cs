1c5d4785c630fe0fbf9ef9d4eddf8e1881f59b47
Francis Kingori <158550717+Uttambsolution@users.noreply.github.com>
2024-05-21 14:38:54 +0300

    Add project files.

---
+++ Everestfcsweb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Everestfcsweb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
